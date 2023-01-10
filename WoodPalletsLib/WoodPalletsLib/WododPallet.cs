202a2a7cb0033bd10c2b2251ab2aa88d3abd1df4
AnthonLUL <90059850+AnthonLUL@users.noreply.github.com>
2023-01-10 13:02:57 +0100

    Add files via upload

---
+++ WoodPalletsLib/WoodPalletsLib/WododPallet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WoodPalletsLib
+{
+    public class WododPallet
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Price { get; set; }
+
+        public int Quality { get; set; }
+
+        public WododPallet(int id, string brand, int price, int quality)
+        {
+            Id = id;
+            Brand = brand;
+            Price = price;
+            Quality = quality;
+        }
+
+        public WododPallet()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Brand = {Brand}, Price = {Price}, Quality = {Quality}";
+        }
+    }
+}
