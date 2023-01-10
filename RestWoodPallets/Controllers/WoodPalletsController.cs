70d4e9c6fb53b0c2ce9ae3ea418be7b931b46e6e
AnthonLUL <90059850+AnthonLUL@users.noreply.github.com>
2023-01-10 13:04:39 +0100

    Add files via upload

---
+++ RestWoodPallets/Controllers/WoodPalletsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWoodPallets.Managers;
+using WoodPalletsLib;
+
+namespace RestWoodPallets.Controllers
+{
+    public class WoodPalletsController : ControllerBase
+    {
+        private readonly WoodPalletsManager _woodPalletsManager;
+
+        public WoodPalletsController()
+        {
+            _woodPalletsManager = new WoodPalletsManager();
+        }
+
+        //get: api/List/Woodpallets
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public IEnumerable<WododPallet> Get()
+        {
+            return _woodPalletsManager.GetAll();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<WododPallet> GetId(int id)
+        {
+            WododPallet wododPallet = _woodPalletsManager.GetById(id);
+            if (wododPallet == null) return NotFound("The Wood Pallet You are looking for doesn't exist");
+            return wododPallet;
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public IActionResult Post([FromBody] WododPallet value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+
+            var woodPallet = _woodPalletsManager.Add(value);
+            return CreatedAtAction(nameof(Get), new { id = woodPallet.Id }, woodPallet);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{id}")]
+        public ActionResult<WododPallet> Put(int id) 
+        {
+            WododPallet woodPallet = _woodPalletsManager.Update(id);
+            if (woodPallet == null) return NotFound("There is no Wood pallets with the given Id");
+            return NoContent();
+        }
+    }
+
+
+}
---
+++ RestWoodPallets/Managers/WoodPalletsManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Linq;
+using WoodPalletsLib;
+
+namespace RestWoodPallets.Managers
+{
+    public class WoodPalletsManager
+    {
+        public static List<WododPallet> WoodPalletsList = new List<WododPallet>()
+        {
+            new WododPallet(1, "Mortensens", 800, 3),
+            new WododPallet(2, "Woodlets", 100, 1),
+            new WododPallet(3, "Woodenheimer", 1300, 5),
+            new WododPallet(4, "Wooster", 2000, 5),
+            new WododPallet(5, "Pallettes", 900, 4),
+            new WododPallet(6, "Hello", 600, 2)
+        };
+
+        public List<WododPallet> GetAll()
+        {
+            return WoodPalletsList;
+        }
+
+        public WododPallet GetById(int id) 
+        {
+            return WoodPalletsList.Find(woodpallet => woodpallet.Id == id);
+        }
+
+        public WododPallet Add(WododPallet newWoodPallet)
+        {
+            newWoodPallet.Id = WoodPalletsList.Count + 1;
+            WoodPalletsList.Add(newWoodPallet);
+            return newWoodPallet;
+        }
+
+        public WododPallet Update(int id)
+        {
+            WododPallet? woodpallet = WoodPalletsList.Find(woodpallet => woodpallet.Id == id);
+            if (woodpallet == null) return null;
+            return woodpallet;
+        }
+    }
+}
